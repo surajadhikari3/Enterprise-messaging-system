e618fe71c3546b728107e469e22902b8756c25af
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2025-01-19 14:47:57 -0500

    fix:file tree tracking

---
+++ src/main/java/io/reactivestax/EMSRestApi/EmsRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.EMSRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reactivestax/EMSRestApi/EmsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reactivestax.EMSRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
